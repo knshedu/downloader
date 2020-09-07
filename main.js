4ebc719c492031f539be06700daf73b4e75cb915
knshedu <70894456+knshedu@users.noreply.github.com>
2020-09-07 21:56:53 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ function files(myName) {
     else{
         if (myName === "5b95cbpv"){
             document.location.href="https://tinyurl.com/yxhn82yt";
-            alert("PDF 檔案開啟教學\n1. 使用共享碼下載檔案\n2. 將檔案開啟（若預設開啟程式為瀏覽器）\n   或將檔案拖移至你專的網頁瀏覽器\n3. 即可瀏覽，Enjoy :)");
+            alert("PDF 檔案開啟教學\n1. 使用共享碼下載檔案\n2. 將檔案開啟（若預設開啟程式為瀏覽器）\n   或將檔案拖移至你使用的網頁瀏覽器\n3. 即可瀏覽，Enjoy :)");
         }
         if (myName === "8a2urxtn"){
             document.location.href="https://tinyurl.com/y26zb8bo";
-            alert("PDF 檔案開啟教學\n1. 使用共享碼下載檔案\n2. 將檔案開啟（若預設開啟程式為瀏覽器）\n   或將檔案拖移至你專的網頁瀏覽器\n3. 即可瀏覽，Enjoy :)");
+            alert("PDF 檔案開啟教學\n1. 使用共享碼下載檔案\n2. 將檔案開啟（若預設開啟程式為瀏覽器）\n   或將檔案拖移至你使用的網頁瀏覽器\n3. 即可瀏覽，Enjoy :)");
         }
         if (myName === "w7vkn8r8"){
             document.location.href="https://tinyurl.com/y5u68ope";
-            alert("PDF 檔案開啟教學\n1. 使用共享碼下載檔案\n2. 將檔案開啟（若預設開啟程式為瀏覽器）\n   或將檔案拖移至你專的網頁瀏覽器\n3. 即可瀏覽，Enjoy :)");
+            alert("PDF 檔案開啟教學\n1. 使用共享碼下載檔案\n2. 將檔案開啟（若預設開啟程式為瀏覽器）\n   或將檔案拖移至你使用的網頁瀏覽器\n3. 即可瀏覽，Enjoy :)");
         }
     }
 }
