c9f7d823e3ade3c5ae14d495c08f501667ce0f72
knshedu <70894456+knshedu@users.noreply.github.com>
2020-09-07 21:24:19 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,24 @@
 var myButton = document.querySelector('button');
-var tf = true;
 
-function notice(){
-    alert("歡迎使用 202 共享專區！\n共享碼請前往班群索取\n\nPDF 檔案開啟教學\n1. 使用共享碼下載檔案\n2. 將檔案開啟（若預設開啟程式為瀏覽器）\n    或將檔案拖移至你專的網頁瀏覽器    \n（Internet Explorer 暫不支援）\n\n3. 即可瀏覽，Enjoy :)")
-}
-
-function files() {
-    let myName = prompt('你輸入你的共享碼');
+function files(myName) {
     if (myName === ""){
         alert("共享碼不得留白");
     }
     else{
-        if (myName === "5b95cbpv-3s4rzp35-qwhmp4w8-yuu92q6j"){
-            document.location.href="https://github.com/knshedu/pdfs/raw/master/knsh-chinese-3-teacher-l1-l3.pdf"
+        if (myName === "5b95cbpv"){
+            document.location.href="https://tinyurl.com/yxhn82yt";
+            alert("PDF 檔案開啟教學\n1. 使用共享碼下載檔案\n2. 將檔案開啟（若預設開啟程式為瀏覽器）\n   或將檔案拖移至你專的網頁瀏覽器\n3. 即可瀏覽，Enjoy :)");
         }
-        if (myName === "8a2urxta-2vtxnfax-3uwp5pzv-gxve4zrn"){
-            document.location.href="https://github.com/knshedu/pdfs/raw/master/knsh-chinese-3-teacher-l4-l6.pdf"
+        if (myName === "8a2urxtn"){
+            document.location.href="https://tinyurl.com/y26zb8bo";
+            alert("PDF 檔案開啟教學\n1. 使用共享碼下載檔案\n2. 將檔案開啟（若預設開啟程式為瀏覽器）\n   或將檔案拖移至你專的網頁瀏覽器\n3. 即可瀏覽，Enjoy :)");
         }
-        if (myName === "w7vkn8r8-xy4fe25w-araby2tn-xw6iqbur"){
-            document.location.href="https://github.com/knshedu/pdfs/raw/master/knsh-chinese-3-teacher-l7-l10.pdf"
+        if (myName === "w7vkn8r8"){
+            document.location.href="https://tinyurl.com/y5u68ope";
+            alert("PDF 檔案開啟教學\n1. 使用共享碼下載檔案\n2. 將檔案開啟（若預設開啟程式為瀏覽器）\n   或將檔案拖移至你專的網頁瀏覽器\n3. 即可瀏覽，Enjoy :)");
         }
-        if (myName === null){
-            tf = false;
+        else{
+            alert("無此共享碼")
         }
     }
-  }
-
-notice()
-while(tf){
-    files()
 }
