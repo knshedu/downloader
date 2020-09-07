7505c12b2041669bf6641d878e960b9def916b42
knshedu <70894456+knshedu@users.noreply.github.com>
2020-09-07 20:31:19 +0800

    Added some notice - 2020/09/07

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var myButton = document.querySelector('button');
 var tf = true;
 
 function notice(){
-    alert("歡迎使用共享專區！\n共享碼請前往班群索取")
+    alert("歡迎使用 202 共享專區！\n共享碼請前往班群索取\n\nPDF 檔案開啟教學\n1. 使用共享碼下載檔案\n2. 將檔案開啟（若預設開啟程式為瀏覽器）\n    或將檔案拖移至你專的網頁瀏覽器    \n（Internet Explorer 暫不支援）\n\n3. 即可瀏覽，Enjoy :)")
 }
 
 function files() {
@@ -12,16 +12,13 @@ function files() {
     }
     else{
         if (myName === "5b95cbpv-3s4rzp35-qwhmp4w8-yuu92q6j"){
-            localStorage.setItem('name', myName);
-            document.location.href="https://github.com/knshedu/etools.s3-ap-northeast-1.amazonaws/raw/master/PDF/" + myName + ".pdf"
+            document.location.href="https://github.com/knshedu/pdfs/raw/master/knsh-chinese-3-teacher-l1-l3.pdf"
         }
         if (myName === "8a2urxta-2vtxnfax-3uwp5pzv-gxve4zrn"){
-            localStorage.setItem('name', myName);
-            document.location.href="https://github.com/knshedu/etools.s3-ap-northeast-1.amazonaws/raw/master/PDF/" + myName + ".pdf"
+            document.location.href="https://github.com/knshedu/pdfs/raw/master/knsh-chinese-3-teacher-l4-l6.pdf"
         }
         if (myName === "w7vkn8r8-xy4fe25w-araby2tn-xw6iqbur"){
-            localStorage.setItem('name', myName);
-            document.location.href="https://github.com/knshedu/etools.s3-ap-northeast-1.amazonaws/raw/master/PDF/" + myName + ".pdf"
+            document.location.href="https://github.com/knshedu/pdfs/raw/master/knsh-chinese-3-teacher-l7-l10.pdf"
         }
         if (myName === null){
             tf = false;
